5ef7b8c8f3703a0673896b4a549b99cdfb6d328a
Makis Tracend <makis.tracend@gmail.com>
2014-07-03 15:31:10 -0700

    :zzz: comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,9 @@ function init(){
 	// require might execute init before the document is ready...
 	//$(document).ready(function(){
 	require(["backbone.app"], function(APP){
-
+		// setup app
 		APP(config.app, function( Controller ){
+			// initialize
 			app = new Controller();
 			// update Backbone options
 			Backbone.history.start({ pushState: config.app.pushState });
